96ba01fd15c8b5b1ed97fa1e9a9b92aa95e50077
jholmquest <jpholmquest@gmail.com>
2021-02-13 13:08:13 -0600

    started building bean

---
+++ src/main/java/bugnet/entity/User.java
@@ -0,0 +1,18 @@
+package bugnet.entity;
+
+/**
+ * class representing a user
+ *
+ * @author James Holmquest
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    /**
+     * Instantiate a new user
+     */
+    public User() {
+    }
+}
