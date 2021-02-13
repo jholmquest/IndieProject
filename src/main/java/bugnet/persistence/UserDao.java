a67b78db58f9fb91c0663fe8c75e9d3c74c6625f
jholmquest <jpholmquest@gmail.com>
2021-02-13 13:49:56 -0600

    get all users method created

--- src/main/java/bugnet/persistence/UserDao.java
+++ src/main/java/bugnet/persistence/UserDao.java
@@ -1,4 +1,32 @@
 package bugnet.persistence;
 
+import bugnet.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.*;
+import java.util.*;
+
 public class UserDao {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /**
+     * Gets all users
+     * @return list of all users
+     */
+    public List<User> getAllUsers() {
+
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+        session.close();
+        return users;
+    }
 }
