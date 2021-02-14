851c56802456cfc410652d25e5f0ef375c04871d
jholmquest <jpholmquest@gmail.com>
2021-02-14 11:41:25 -0600

    sql file for testing

---
+++ src/test/resources/cleanUser.sql
@@ -0,0 +1,5 @@
+delete from user;
+
+insert into user values (1, 'testuser', 'password');
+insert into user values (2, 'anotheruser', '123pass');
+insert into user values (3, 'holmquest', 'aaaaaaaa');
