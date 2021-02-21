30213041ad8c5b8d52e590765edd95b6641a3a65
jholmquest <jpholmquest@gmail.com>
2021-02-21 15:11:15 -0600

    jdoc

--- src/main/java/bugnet/entity/Specimen.java
+++ src/main/java/bugnet/entity/Specimen.java
@@ -34,9 +34,20 @@ public class Specimen {
     )
     private User user;
 
+    /**
+     * Standard no arg constructor
+     */
     public Specimen() {
     }
 
+    /**
+     * creates a specimen with specified parameters
+     * @param bugName name of insect
+     * @param collectedLocation where insect was collected
+     * @param collectedDate when insect was collected
+     * @param bugNotes notes about insect
+     * @param user user who collected insect
+     */
     public Specimen(String bugName, String collectedLocation,
                     Date collectedDate, String bugNotes, User user) {
         this.bugName = bugName;
@@ -46,50 +57,98 @@ public class Specimen {
         this.user = user;
     }
 
+    /**
+     * gets the id
+     * @return id of insect
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * sets the id
+     * @param id id of insect
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * gets the name of the insect
+     * @return name of insect
+     */
     public String getBugName() {
         return bugName;
     }
 
+    /**
+     * sets the name of the insect
+     * @param bugName name of insect
+     */
     public void setBugName(String bugName) {
         this.bugName = bugName;
     }
 
+    /**
+     * gets where insect was collected
+     * @return where insect was collected
+     */
     public String getCollectedLocation() {
         return collectedLocation;
     }
 
+    /**
+     * sets where insect was collected
+     * @param collectedLocation where insect was collected
+     */
     public void setCollectedLocation(String collectedLocation) {
         this.collectedLocation = collectedLocation;
     }
 
+    /**
+     * gets date insect collected
+     * @return date insect collected
+     */
     public Date getCollectedDate() {
         return collectedDate;
     }
 
+    /**
+     * sets date insect collected
+     * @param collectedDate
+     */
     public void setCollectedDate(Date collectedDate) {
         this.collectedDate = collectedDate;
     }
 
+    /**
+     * gets notes on insect
+     * @return notes on insect
+     */
     public String getBugNotes() {
         return bugNotes;
     }
 
+    /**
+     * sets notes on insect
+     * @param bugNotes notes on insect
+     */
     public void setBugNotes(String bugNotes) {
         this.bugNotes = bugNotes;
     }
 
+    /**
+     * gets user that collected insect
+     * @return collector
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * sets user that collected insect
+     * @param user collector
+     */
     public void setUser(User user) {
         this.user = user;
     }
