141df6167bdb3e1aa0673ea4a634ce2d13c7b5c8
jholmquest <jpholmquest@gmail.com>
2021-02-21 12:01:55 -0600

    one to many established

--- src/main/java/bugnet/entity/Specimen.java
+++ src/main/java/bugnet/entity/Specimen.java
@@ -1,7 +1,7 @@
 package bugnet.entity;
 
 import org.hibernate.annotations.GenericGenerator;
-
+import java.util.Date;
 import javax.persistence.*;
 
 /**
@@ -10,8 +10,27 @@ import javax.persistence.*;
 @Entity(name = "Specimen")
 @Table(name = "specimen")
 public class Specimen {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    @Column(name = "determination")
+    private String bugName;
+
+    @Column(name = "location")
+    private String collectedLocation;
+
+    @Column(name = "date")
+    private Date collectedDate;
+
+    @Column(name = "notes")
+    private String bugNotes;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",
+            foreignKey = @ForeignKey(name = "specimen_user_id_fk")
+    )
+    private User user;
 }
