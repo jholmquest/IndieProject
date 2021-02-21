8dfdfa60b1bc973a6d46f0524615818922a1ea85
jholmquest <jpholmquest@gmail.com>
2021-02-21 12:55:22 -0600

    sql file for testing purposes

---
+++ src/test/resources/cleanSpecimen.sql
@@ -0,0 +1,5 @@
+delete from specimen;
+
+insert into specimen values (1, 'beetle', 'madison', '1997-01-01', 'eating some grass', 3);
+insert into specimen values (2, 'wasp', 'milwaukee', '2004-04-17', 'predator of buprestid', 3);
+insert into specimen values (3, 'fly', 'shawano', '2013-07-18', 'buzzing about', 2);
