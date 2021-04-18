70d2f73b9fabb836a34e1d667aa874df71157ded
jholmquest <jpholmquest@gmail.com>
2021-04-18 15:50:59 -0500

    created class to generate a map containing location info

--- src/main/java/bugnet/controller/LocationBuilder.java
+++ src/main/java/bugnet/controller/LocationBuilder.java
@@ -1,9 +1,24 @@
 package bugnet.controller;
 
+import bugnet.api.Geocode;
+import bugnet.persistence.PropertyLoader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class LocationBuilder {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private final PropertyLoader loader = new PropertyLoader();
+    private final Properties properties = loader.loadProperties("/api.properties");
 
     // need to replace certain characters for the api
     public String escapeLocation(String locationName) {
@@ -14,11 +29,34 @@ public class LocationBuilder {
     }
 
     public Map<LocationKey, String> getCoordinates(String locationName) {
-        LocationKey key = LocationKey.FOUND;
-        String value = "value";
 
+        Client client = ClientBuilder.newClient();
         Map<LocationKey, String> map = new HashMap<>();
-        map.put(key, value);
+
+        try {
+            WebTarget target = client.target(
+                    properties.getProperty("request.url") +
+                            escapeLocation(locationName) +
+                            properties.getProperty("api.key"));
+            String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+            ObjectMapper mapper = new ObjectMapper();
+            Geocode results = mapper.readValue(response, Geocode.class);
+
+            String found = results.getStatus();
+            String latitude = String.valueOf(results.getResults().get(0).getGeometry().getLocation().getLat());
+            String longitude = String.valueOf(results.getResults().get(0).getGeometry().getLocation().getLng());
+            map.put(LocationKey.FOUND, found);
+            map.put(LocationKey.LATITUDE, latitude);
+            map.put(LocationKey.LONGITUDE, longitude);
+
+        } catch(Exception e) {
+            logger.error(e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+
         return map;
     }
 }
