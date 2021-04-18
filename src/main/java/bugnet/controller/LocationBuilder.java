c5404fad41b0d1f17e9949243732d5afcbe89a47
jholmquest <jpholmquest@gmail.com>
2021-04-18 15:24:32 -0500

    added method to escape spaces

--- src/main/java/bugnet/controller/LocationBuilder.java
+++ src/main/java/bugnet/controller/LocationBuilder.java
@@ -1,4 +1,19 @@
 package bugnet.controller;
 
 public class LocationBuilder {
+
+    private String locationName;
+    public LocationBuilder() {
+
+    }
+
+    public LocationBuilder(String locationName) {
+        this.locationName = escapeLocation(locationName);
+    }
+
+    public String escapeLocation(String locationName) {
+        return locationName.replace(" ", "%20");
+    }
+
+
 }
