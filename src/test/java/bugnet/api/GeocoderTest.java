9b3d0e8e49adc67cdca5e4e329dde15ab31d78d9
jholmquest <jpholmquest@gmail.com>
2021-04-18 20:42:34 -0500

    fully testing the location builder

--- src/test/java/bugnet/api/GeocoderTest.java
+++ src/test/java/bugnet/api/GeocoderTest.java
@@ -15,27 +15,10 @@ import java.util.Properties;
 
 public class GeocoderTest {
 
-    private final PropertyLoader loader = new PropertyLoader();
-    private final Properties properties = loader.loadProperties("/api.properties");
-
-    @Test
-    public void makeRequest() throws Exception {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(properties.getProperty("request.url") + "Madison,%20WI" +
-                properties.getProperty("api.key"));
-        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
-        ObjectMapper mapper = new ObjectMapper();
-        Geocode results = mapper.readValue(response, Geocode.class);
-
-        double expectedLatitude = 43.0730517;
-        double expectedLongitude = -89.4012302;
-        assertEquals(expectedLatitude, results.getResults().get(0).getGeometry().getLocation().getLat());
-        assertEquals(expectedLongitude, results.getResults().get(0).getGeometry().getLocation().getLng());
-    }
+    private static final LocationBuilder builder = new LocationBuilder();
 
     @Test
     public void testBuilder() {
-        LocationBuilder builder = new LocationBuilder();
         List<ResultsItem> results = builder.getCoordinates("Madison, WI");
         double expectedLatitude = 43.0730517;
         double expectedLongitude = -89.4012302;
@@ -45,10 +28,13 @@ public class GeocoderTest {
 
     @Test
     public void testVoid() {
-        LocationBuilder builder = new LocationBuilder();
         List<ResultsItem> results = builder.getCoordinates("feagaega;eaveeofeowa");
         assertEquals(0, results.size());
     }
 
-
+    @Test
+    public void testEscape() {
+        String escape = builder.escapeLocation("A B %");
+        assertEquals("A%20B%20%25", escape);
+    }
 }
