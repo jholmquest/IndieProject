6075f1671b50bb5f32ff70b32a65dbee706674fe
jholmquest <jpholmquest@gmail.com>
2021-04-20 14:40:50 -0500

    created some tests

--- src/test/java/bugnet/controller/AddUserTest.java
+++ src/test/java/bugnet/controller/AddUserTest.java
@@ -1,4 +1,31 @@
 package bugnet.controller;
 
+import bugnet.entity.User;
+import bugnet.persistence.Database;
+import bugnet.persistence.GenericDao;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AddUserTest {
+
+    private static final AddUser add = new AddUser();
+    /**
+     * cleans db, makes a new dao
+     */
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("clean.sql");
+    }
+
+    @Test
+    public void uniqueUsernameTest() {
+        assertTrue(add.uniqueUsername("afeaeafefewafaweawef"));
+    }
+
+    @Test
+    public void repeatUsernameTest() {
+        assertFalse(add.uniqueUsername("testuser"));
+    }
 }
