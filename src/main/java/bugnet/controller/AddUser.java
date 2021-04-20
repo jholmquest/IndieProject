aeed5df8046c296a02f1a009e76905c209b58e05
jholmquest <jpholmquest@gmail.com>
2021-04-20 14:49:28 -0500

    added feedback, jdoc

--- src/main/java/bugnet/controller/AddUser.java
+++ src/main/java/bugnet/controller/AddUser.java
@@ -3,28 +3,64 @@ package bugnet.controller;
 import bugnet.entity.Role;
 import bugnet.entity.User;
 import bugnet.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
+/**
+ * Holds the logic necessary for creating a user
+ *
+ * @author James Holmquest
+ */
 public class AddUser {
 
+    private final Logger logger = LogManager.getLogger(this.getClass());
     private GenericDao<User> dao;
 
     public AddUser() {
         dao = new GenericDao<>(User.class);
     }
+
+    /**
+     * checks if a username is unique
+     * @param username username being checked
+     * @return true if no username found, otherwise false
+     */
     public boolean uniqueUsername(String username) {
         List<User> users = dao.findByPropertyEqual("username", username);
 
         return users.isEmpty();
     }
 
-    public void add(User newUser) {
-        int id = dao.insert(newUser);
-        newUser.setId(id);
-        initializeRole(newUser);
+    /**
+     * Attempts to add a user, if successful also triggers method to set their role
+     * @param newUser User to be added
+     * @return message to be output based on results
+     */
+    public String add(User newUser) {
+
+        if (uniqueUsername(newUser.getUsername())) {
+            int id = dao.insert(newUser);
+
+            if (id != 0) {
+                newUser.setId(id);
+                initializeRole(newUser);
+                return "User created successfully";
+            } else {
+
+                logger.error("There was an issue adding the user");
+                return "Something went wrong";
+            }
+        } else {
+            return "That username was taken";
+        }
     }
 
+    /**
+     * Gives a new user the initial role of user
+     * @param newUser newly created user
+     */
     public void initializeRole(User newUser) {
 
         Role initialRole = new Role("user", newUser.getUsername(), newUser);
