bd67179cdeb9313951abd8d17c2b2ede3c1bb1cd
jholmquest <jpholmquest@gmail.com>
2021-04-20 14:56:35 -0500

    test for adding repeat user created

--- src/test/java/bugnet/controller/AddUserTest.java
+++ src/test/java/bugnet/controller/AddUserTest.java
@@ -5,11 +5,15 @@ import bugnet.persistence.Database;
 import bugnet.persistence.GenericDao;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddUserTest {
 
     private static final AddUser add = new AddUser();
+    private static final GenericDao<User> userDao = new GenericDao<>(User.class);
     /**
      * cleans db, makes a new dao
      */
@@ -27,5 +31,20 @@ public class AddUserTest {
     @Test
     public void repeatUsernameTest() {
         assertFalse(add.uniqueUsername("testuser"));
+    }
+
+    @Test
+    public void repeatUserTest() {
+        User repeatUser = new User("testuser", "pasword");
+        String message = add.add(repeatUser);
+        List<User> users = userDao.getAll();
+
+        assertEquals(3, users.size());
+        assertEquals("That username was taken", message);
+    }
+
+    @Test
+    public void newUserTest() {
+
     }
 }
