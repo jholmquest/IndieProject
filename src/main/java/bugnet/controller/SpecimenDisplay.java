283a549d92d90291b4f3a7763e0b7bd89da48238
jholmquest <jpholmquest@gmail.com>
2021-04-23 17:26:29 -0500

    added servlet for displaying specimens

--- src/main/java/bugnet/controller/SpecimenDisplay.java
+++ src/main/java/bugnet/controller/SpecimenDisplay.java
@@ -1,4 +1,31 @@
 package bugnet.controller;
 
-public class SpecimenDisplay {
+import bugnet.entity.Specimen;
+import bugnet.entity.User;
+import bugnet.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(
+        urlPatterns = {"/bugs"}
+)
+public class SpecimenDisplay extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        GenericDao<Specimen> dao = new GenericDao<>(Specimen.class);
+        List<Specimen> specimens = dao.getAll();
+
+        req.setAttribute("specimens", specimens);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/specimenDisplay.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
---
+++ src/main/webapp/specimenDisplay.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="head.jsp" />
+<html>
+<body>
+<table>
+    <tr>
+        <th>Specimen Name</th>
+    </tr>
+    <c:forEach var="specimen" items="${specimens}">
+        <tr>
+            <td>${specimen.bugname}</td>
+        </tr>
+    </c:forEach>
+
+</table>
+</body>
+</html>
