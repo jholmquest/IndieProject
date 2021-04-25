77e5db956df5c015222b173bfcc3b7af11ffa599
jholmquest <jpholmquest@gmail.com>
2021-04-25 17:46:45 -0500

    added specimen insertion

--- src/main/java/bugnet/controller/AddSpecimen.java
+++ src/main/java/bugnet/controller/AddSpecimen.java
@@ -1,9 +1,43 @@
 package bugnet.controller;
 
+import bugnet.entity.Specimen;
+import bugnet.entity.User;
+import bugnet.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 @WebServlet(
         urlPatterns = {"/newBug"}
 )
-public class AddSpecimen {
+public class AddSpecimen extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/addSpecimen.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LocalDate collectedDate = LocalDate.parse(req.getParameter("collectedDate"));
+        User user = (User)req.getSession().getAttribute("sessionUser");
+        Specimen newSpecimen = new Specimen(
+                req.getParameter("determination"),
+                req.getParameter("location"),
+                collectedDate,
+                req.getParameter("notes"),
+                user);
+
+        GenericDao<Specimen> dao = new GenericDao<>(Specimen.class);
+        dao.insert(newSpecimen);
+        user.addSpecimen(newSpecimen);
+    }
 }
--- src/main/webapp/addSpecimen.jsp
+++ src/main/webapp/addSpecimen.jsp
@@ -5,15 +5,15 @@
 <c:import url="template/header.jsp" />
 <main>
     <c:import url="template/nav.jsp" />
-<form action="#" method="POST">
+<form action="newBug" method="POST">
     <label for="determination">What is it?</label>
-    <input type="text" id="determination">
+    <input type="text" id="determination" name="determination">
     <label for="location">Where did you collect it?</label>
-    <input type="text" id="location">
+    <input type="text" id="location" name="location">
     <label for="collectedDate">When did you collect it?</label>
-    <input type="date" id="collectedDate">
+    <input type="date" id="collectedDate" name="collectedDate">
     <label for="notes">Notes</label>
-    <textarea id="notes">Enter additional notes...</textarea>
+    <textarea id="notes" name="notes">Enter additional notes...</textarea>
     <button type="submit" name="submitSpecimen">Submit</button>
 </form>
 </main>
