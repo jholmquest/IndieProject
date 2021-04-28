1a6f4f8133a0422c9ebbec3569756fbb5d28cce3
jholmquest <jpholmquest@gmail.com>
2021-04-27 19:25:29 -0500

    added a little jdoc

--- src/main/java/bugnet/controller/GenerateCoordinates.java
+++ src/main/java/bugnet/controller/GenerateCoordinates.java
@@ -14,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Generates coordinates based on location listed in the table
+ */
 @WebServlet(
         urlPatterns = {"/coordinates"}
 )
