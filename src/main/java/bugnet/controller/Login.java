e60a6c9bc008ddf62db63736d426150eb2e71ad2
jholmquest <jpholmquest@gmail.com>
2021-05-04 14:54:53 -0500

    screenshots updated

--- src/main/java/bugnet/controller/Login.java
+++ src/main/java/bugnet/controller/Login.java
@@ -12,13 +12,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Sets up environment after successful login
+ *
+ * @author James Holmquest
+ */
 @WebServlet(
         urlPatterns = "/login"
 )
 public class Login extends HttpServlet {
 
+    /**
+     * Adds current user to the session
+     * @param req http request
+     * @param resp http response
+     * @throws IOException issue with input/output
+     */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         GenericDao<User> dao = new GenericDao<>(User.class);
         List<User> user = dao.findByPropertyEqual(Column.USERNAME, req.getRemoteUser());
--- src/main/java/bugnet/controller/Logout.java
+++ src/main/java/bugnet/controller/Logout.java
@@ -10,15 +10,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
+/**
+ * Logs user out by destroying the session
+ *
+ * @author James Holmquest
+ */
 @WebServlet(
         urlPatterns = "/logout"
 )
 public class Logout extends HttpServlet {
 
+    /**
+     * destroys the session and brings user back to home
+     * @param req http request
+     * @param resp http response
+     * @throws IOException issue with input/output
+     */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         req.getSession().invalidate();
         resp.sendRedirect(".");
