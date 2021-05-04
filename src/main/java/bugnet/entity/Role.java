312c6eb64c3cf1e6e7c5dbfcb28d4ab35ba461e3
jholmquest <jpholmquest@gmail.com>
2021-05-04 15:01:02 -0500

    jdoc

--- src/main/java/bugnet/entity/Role.java
+++ src/main/java/bugnet/entity/Role.java
@@ -34,15 +34,29 @@ public class Role {
     )
     private User user;
 
+    /**
+     * basic no argument constructor
+     */
     public Role() {
     }
 
+    /**
+     * constructs a Role object with preset values
+     * @param roleName name of role
+     * @param userName user with role
+     * @param user reference to user for hibernate relational mapping
+     */
     public Role(String roleName, String userName, User user) {
         this.roleName = roleName;
         this.username = userName;
         this.user = user;
     }
 
+    /**
+     * Checks if two role objects are equivalent
+     * @param o role object being passed in
+     * @return whether or not the objects are equivalent
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -51,6 +65,10 @@ public class Role {
         return id == role.id && Objects.equals(roleName, role.roleName) && Objects.equals(username, role.username);
     }
 
+    /**
+     * gives the hash code
+     * @return hash code
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id, roleName, username);
