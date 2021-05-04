e2a3496076707b02752bc783e37acc388c64c347
jholmquest <jpholmquest@gmail.com>
2021-05-03 22:47:34 -0500

    testing setCoordinate completed, other methods require request and can't really be tested properly

--- src/test/java/bugnet/util/InputControllerTest.java
+++ src/test/java/bugnet/util/InputControllerTest.java
@@ -1,8 +1,31 @@
 package bugnet.util;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class InputControllerTest implements InputController{
+
+    @Test
+    void emptyStringTest() {
+        Double nullDouble = setCoordinate("");
+        assertNull(nullDouble);
+    }
+
+    /**
+     * tests if a negative number is parsed correctly
+     */
+    @Test
+    void negativeStringTest() {
+        Double negativeDouble = setCoordinate("-10.1");
+        // assertEquals requires primitive while class uses wrapper so null values can be added
+        assertEquals(-10.1, negativeDouble.doubleValue());
+    }
+
+    @Test
+    void positiveStringTest() {
+        Double positiveDouble = setCoordinate("+10.1");
+        assertEquals(10.1, positiveDouble.doubleValue());
+    }
 }
