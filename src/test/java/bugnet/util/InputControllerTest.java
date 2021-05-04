ed00f9b1823f9cc1a9642e9cfc63cd38df95d2c6
jholmquest <jpholmquest@gmail.com>
2021-05-03 22:41:00 -0500

    testing the testable method in InputController interface

---
+++ src/test/java/bugnet/util/InputControllerTest.java
@@ -0,0 +1,8 @@
+package bugnet.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class InputControllerTest implements InputController{
+}
